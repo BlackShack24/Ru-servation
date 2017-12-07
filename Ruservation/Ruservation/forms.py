0855245500434da196a800484a567b0adc7e60ed
clément spies <spy_c@hotmail.fr>
2017-12-07 11:36:36 +0100

    Test connexion

---
+++ Ruservation/Ruservation/forms.py
@@ -0,0 +1,3 @@
+class ConnexionForm(forms.Form):
+    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
