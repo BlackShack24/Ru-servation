d19f04ce7d0ee6170b633ddb143faad7ad919ff4
clément spies <spy_c@hotmail.fr>
2017-12-07 11:57:51 +0100

    Login qui fonctioooonne

--- Ruservation/appPrincipale/urls.py
+++ Ruservation/appPrincipale/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'appPrincipale/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
