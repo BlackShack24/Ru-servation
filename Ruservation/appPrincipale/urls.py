2cd20723a9cda48601529ff9d65aba259129478e
Gautier <gautier.dran@orange.fr>
2017-12-14 11:28:40 +0100

    la connexion marche

--- Ruservation/appPrincipale/urls.py
+++ Ruservation/appPrincipale/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'appPrincipale/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^home/$', views.home, name='home'),
+    #url(r'^admin/', admin.site.urls),
 ]
