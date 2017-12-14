a88c650b2512878793eab996e915dc31eecc5c37
clément spies <spy_c@hotmail.fr>
2017-12-14 09:52:37 +0100

    Yeah

--- Ruservation/appPrincipale/urls.py
+++ Ruservation/appPrincipale/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'appPrincipale/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
 ]
