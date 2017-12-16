864e8a380436203a7768ed88dc8831592be3bf14
clément spies <spy_c@hotmail.fr>
2017-12-16 23:18:25 +0100

    Inscription

--- Ruservation/appPrincipale/urls.py
+++ Ruservation/appPrincipale/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from appPrincipale import views as appPrincipale_views
 from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'appPrincipale/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'appPrincipale/login.html'}, name='logout'),
-    url(r'^$', TemplateView.as_view(template_name='appPrincipale/home.html'), name='home'),
+    url(r'^home/$', TemplateView.as_view(template_name='appPrincipale/home.html'), name='home'),
+    url(r'^signup/$', appPrincipale_views.signup, name='signup'),
     #url(r'^home/$', views.home, name='home'),
     #url(r'^admin/', admin.site.urls),
 ]
--- Ruservation/appPrincipale/views.py
+++ Ruservation/appPrincipale/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from Ruservation.forms import SignUpForm
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, World")
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'appPrincipale/signup.html', {'form': form})
