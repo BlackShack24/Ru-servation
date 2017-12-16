267d22a70c1c221292b6827019e0482c62173f09
clément spies <spy_c@hotmail.fr>
2017-12-16 22:17:54 +0100

    Deconnexion

--- Ruservation/appPrincipale/urls.py
+++ Ruservation/appPrincipale/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'appPrincipale/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'appPrincipale/login.html'}, name='logout'),
+    url(r'^$', TemplateView.as_view(template_name='appPrincipale/home.html'), name='home'),
+    #url(r'^home/$', views.home, name='home'),
     #url(r'^admin/', admin.site.urls),
 ]
