4dff981a07084427749243aac2a39475a031167e
clément spies <spy_c@hotmail.fr>
2017-12-17 20:12:26 +0100

    Création des models

--- Ruservation/appPrincipale/models.py
+++ Ruservation/appPrincipale/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
 # Create your models here.
+class Entree(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    points = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nom
+
+class PlatPrincipal(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    points = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nom
+
+class Fromage(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    points = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nom        
+
+class Dessert(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    points = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nom
+
+class Boisson(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    points = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nom   
+
+class LieuRestauration(models.Model):
+    nom = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=100)
+    TYPE = (
+     ('RU', 'Restaurant Universitaire'),
+     ('BR', 'Brasserie'),
+     ('CA', 'Cafétéria'),
+     ('FO', 'Foodtruck'),
+     ('SA', 'Salle administrative'),
+     ('SW', 'Sandwicherie'),
+    )
+    tempsAttente = models.IntegerField(null=True)
+    note = models.FloatField(null=True)
+    distance = models.FloatField(null=True)
+    platPrincipal = models.ManyToManyField(PlatPrincipal, through='Menu')
+    entree = models.ManyToManyField(Entree, through='Menu')
+    fromage = models.ManyToManyField(Fromage, through='Menu')
+    dessert = models.ManyToManyField(Dessert, through='Menu')
+    boisson = models.ManyToManyField(Boisson, through='Menu')
+    
+
+    def __str__(self):
+        return self.nom             
+
+class Menu(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    lieuRestauration = models.ForeignKey(LieuRestauration)
+    platPrincipal = models.ForeignKey(PlatPrincipal)
+    entree = models.ForeignKey(Entree, null=True)
+    fromage = models.ForeignKey(Fromage, null=True)
+    dessert = models.ForeignKey(Dessert, null=True)
+    boisson = models.ForeignKey(Boisson, null=True)
+    pain = models.NullBooleanField()
+
+    def __str__(self):
+        return self.nom
+
